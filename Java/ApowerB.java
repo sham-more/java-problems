d10e62bd14fdd210fd5429846b15b291d9fee1f1
sham-more <sgmore456@gmail.com>
2023-09-02 22:46:36 +0530

    Added program

---
+++ Java/ApowerB.java
@@ -0,0 +1,30 @@
+ 
+
+public class ApowerB
+{
+	public static void main(String[] args) {
+	    
+	   
+	   
+	   int a =3;
+	   int b = 4;
+	   
+	   int ans =1;
+	   
+	   
+	   for( int i=1; i <= b ; i++){
+	       
+	       ans  = ans * a ;
+	   }
+	   
+	   System.out.println(ans);
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+ 	}
+}
